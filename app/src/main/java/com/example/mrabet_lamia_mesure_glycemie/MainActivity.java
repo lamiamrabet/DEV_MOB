75d9d6048b6aed09b9bb5b91dd02c59ae0471c07
Lamia Mrabet <lamiamrabet456@gmail.com>
2023-11-01 12:39:20 +0100

    initial commit

---
+++ app/src/main/java/com/example/mrabet_lamia_mesure_glycemie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mrabet_lamia_mesure_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
