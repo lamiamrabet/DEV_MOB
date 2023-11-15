31e2a70bbfc00ee1fe52cecb81c97cddf95405d3
Lamia Mrabet <lamiamrabet456@gmail.com>
2023-11-15 15:31:35 +0100

    tp4 (act1,2)

--- app/src/main/java/com/example/mrabet_lamia_mesure_glycemie/controller/controller.java
+++ app/src/main/java/com/example/mrabet_lamia_mesure_glycemie/controller/controller.java
@@ -1,4 +1,15 @@
 package com.example.mrabet_lamia_mesure_glycemie.controller;
-
+import com.example.mrabet_lamia_mesure_glycemie.modele.patient;
 public class controller {
+    private static patient patient;
+    public controller()
+    {
+        super();
+    }
+    public void create_patient(int age ,float valeurMesuree , boolean isFasting){
+        patient = new patient(isFasting, age, valeurMesuree);
+    }
+    public String getResult(){
+        return patient.getR();//update patient to controller
+    }
 }
--- app/src/main/java/com/example/mrabet_lamia_mesure_glycemie/modele/patient.java
+++ app/src/main/java/com/example/mrabet_lamia_mesure_glycemie/modele/patient.java
@@ -1,4 +1,67 @@
 package com.example.mrabet_lamia_mesure_glycemie.modele;
 
+
 public class patient {
+    private boolean isFasting;
+    private int age;
+    private float valeurMesuree;
+    private static String r;
+
+    public patient(boolean isFasting, int age, float valeurMesuree) {
+        this.isFasting = isFasting;
+        this.age = age;
+        this.valeurMesuree = valeurMesuree;
+        calculer();
+    }
+
+    public void calculer () {
+
+            if (isFasting) {
+                if (age >= 13) {
+                    if (valeurMesuree < 5.0)
+                        r="Niveau de glycémie est trop bas";
+                    else if (valeurMesuree >= 5.0 && valeurMesuree <= 7.2)
+                        r="Niveau de glycémie est normale";
+                    else
+                        r="Niveau de glycémie est trop élevé";
+                } else if (age >= 6 && age <= 12) {
+                    if (valeurMesuree < 5.0)
+                        r="Niveau de glycémie est trop bas";
+                    else if (valeurMesuree >= 5.0 && valeurMesuree <= 10.0)
+                        r="Niveau de glycémie est normale";
+                    else
+                        r="Niveau de glycémie est trop élevé";
+                } else if (age < 6) {
+                    if (valeurMesuree < 5.5)
+                        r="Niveau de glycémie est trop bas";
+                    else if (valeurMesuree >= 5.5 && valeurMesuree <= 10.0)
+                        r="Niveau de glycémie est normale";
+                    else
+                        r="Niveau de glycémie est trop élevé";
+                }
+            } else {
+                if (valeurMesuree > 10.5)
+                    r="Niveau de glycémie est trop élevé";
+                else
+                    r="Niveau de glycémie est normale";
+            }
+        }
+
+    public boolean isFasting() {
+        return isFasting;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public float getValeurMesuree() {
+        return valeurMesuree;
+    }
+
+    public static String getR() {
+        return r;
+    }
 }
+
+
